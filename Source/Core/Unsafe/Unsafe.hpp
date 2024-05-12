f1819520294f885e9e42de2867a344231cda606a
raygardnercpp <samuelalonso26@icloud.com>
2024-05-12 19:22:16 +0100

    Fixed Unsafe

--- Source/Core/Unsafe/Unsafe.hpp
+++ Source/Core/Unsafe/Unsafe.hpp
@@ -1,5 +1,5 @@
 #ifndef lucy_core_unsafe_unsafe
 #define lucy_core_unsafe_unsafe
-#define unreliable(...) (::Unsafe __VA_OPT__(,) __VA_ARGS__)
-#define unsafe(...) (::Unsafe{} __VA_OPT__(,) __VA_ARGS__)
+#define lucy_unreliable(...) (::Unsafe __VA_OPT__(,) __VA_ARGS__)
+#define lucy_unsafe(...) (::Unsafe{} __VA_OPT__(,) __VA_ARGS__)
 #endif
