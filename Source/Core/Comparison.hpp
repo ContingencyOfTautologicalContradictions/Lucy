0b3a41b1dc3685f84a3b711bb8f23c3823c81156
raygardnercpp <samuelalonso26@icloud.com>
2024-08-03 01:17:43 +0100

    Added Core Comparison

--- Source/Core/Comparison.hpp
+++ Source/Core/Comparison.hpp
@@ -1,3 +1,4 @@
+#if lucy_major >= 0 and lucy_middle >= 0 and lucy_minor >= 1
 #ifndef lucy_core_comparison
 #define lucy_core_comparison
 
@@ -11,4 +12,5 @@ namespace Lucy
     };
 }
 
+#endif
 #endif
