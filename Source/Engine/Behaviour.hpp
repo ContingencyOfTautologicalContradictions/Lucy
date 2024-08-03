875bdcba338d4371b4d2377b4e1638af5398c9c9
raygardnercpp <samuelalonso26@icloud.com>
2024-08-03 01:38:18 +0100

    Added Engine Behaviour

---
+++ Source/Engine/Behaviour.hpp
@@ -0,0 +1,11 @@
+#if lucy_major >= 0 and lucy_middle >= 0 and lucy_minor >= 1
+#ifndef lucy_engine_behaviour
+#define lucy_engine_behaviour
+
+namespace Lucy::Engine
+{
+    class [[nodiscard]] Behaviour{};
+}
+
+#endif
+#endif
