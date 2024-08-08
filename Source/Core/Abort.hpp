4d9cbddafd177a434ddacbce82e17432ea4ba155
raygardnercpp <samuelalonso26@icloud.com>
2024-08-08 20:00:01 +0100

    Added Core Abort

--- Source/Core/Abort.hpp
+++ Source/Core/Abort.hpp
@@ -1,10 +1,20 @@
 #if lucy_major >= 0 and lucy_middle >= 0 and lucy_minor >= 1
 #ifndef lucy_core_abort
 #define lucy_core_abort
+#ifdef _MSC_VER
+#include <intrin.h>
+#endif
 
 namespace Lucy
 {
-    [[noreturn]] auto Abort() noexcept -> void;
+    [[noreturn]] inline auto Abort() noexcept -> void
+    {
+        #ifdef _MSC_VER
+        __fastfail(0);
+        #else
+        __builtin_trap();
+        #endif
+    }
 }
 
 #endif
