3da70998a5dd8c4d3c03db7549f65ecff90a83db
raygardnercpp <samuelalonso26@icloud.com>
2024-08-15 03:19:29 +0100

    Added Core Size

---
+++ Source/Core/Size.hpp
@@ -0,0 +1,11 @@
+#if lucy_major >= 0 and lucy_middle >= 0 and lucy_minor >= 1
+#ifndef lucy_core_size
+#define lucy_core_size
+
+namespace Lucy
+{
+    using Size = decltype(sizeof(bool));
+}
+
+#endif
+#endif
