7e8d129f2fea2ff157f375ef5754dfbc46fba1c9
raygardnercpp <samuelalonso26@icloud.com>
2024-08-15 03:19:47 +0100

    Added Core Unsafe

---
+++ Source/Core/Unsafe.hpp
@@ -0,0 +1,27 @@
+#if lucy_major >= 0 and lucy_middle >= 0 and lucy_minor >= 1
+#ifndef lucy_core_unsafe
+#define lucy_core_unsafe
+#define lucy_unreliable(...) ([[maybe_unused]] ::Lucy::Detail::Unsafe __VA_OPT__(,) __VA_ARGS__)
+#define lucy_unsafe(...) (::Lucy::Detail::Unsafe{} __VA_OPT__(,) __VA_ARGS__)
+
+namespace Lucy::Detail
+{
+    class [[nodiscard]] Unsafe final
+    {
+    public:
+        inline constexpr ~Unsafe() noexcept = default;
+
+        inline constexpr Unsafe() noexcept = default;
+
+        constexpr Unsafe(const Unsafe&) noexcept = delete;
+
+        constexpr Unsafe(Unsafe&&) noexcept = delete;
+
+        constexpr auto operator=(const Unsafe&) noexcept -> Unsafe& = delete;
+
+        constexpr auto operator=(Unsafe&&) noexcept -> Unsafe& = delete;
+    };
+}
+
+#endif
+#endif
