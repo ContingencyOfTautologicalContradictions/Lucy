3f299a4ff3733cb9b5720b29a01f958a193730d8
raygardnercpp <samuelalonso26@icloud.com>
2024-08-17 09:33:39 +0100

    Added Core u8

--- Source/Core/u8.hpp
+++ Source/Core/u8.hpp
@@ -32,120 +32,120 @@ namespace Lucy
     public:
         using Native = unsigned char;
 
-        [[nodiscard]] inline consteval auto Bits() noexcept -> u8
+        [[nodiscard]] static consteval auto Bits() noexcept -> u8
         {
             return u8 lucy_unsafe(8u);
         }
 
-        [[nodiscard]] inline consteval auto Minimum() noexcept -> u8
+        [[nodiscard]] static consteval auto Minimum() noexcept -> u8
         {
             return u8 lucy_unsafe(0u);
         }
 
-        [[nodiscard]] inline consteval auto Maximum() noexcept -> u8
+        [[nodiscard]] static consteval auto Maximum() noexcept -> u8
         {
             return u8 lucy_unsafe(255u);
         }
 
-        inline constexpr ~u8() noexcept = default;
+        constexpr ~u8() noexcept = default;
 
-        inline constexpr u8() noexcept : m_value(0u){}
+        constexpr u8() noexcept : m_value(0u){}
 
-        inline constexpr u8(const u8& other) noexcept = default;
+        constexpr u8(const u8& other) noexcept = default;
 
-        inline constexpr u8(u8&& other) noexcept = default;
+        constexpr u8(u8&& other) noexcept = default;
 
-        inline constexpr auto operator=(const u8& other) noexcept -> u8& = default;
+        constexpr auto operator=(const u8& other) noexcept -> u8& = default;
 
-        inline constexpr auto operator=(u8&& other) noexcept -> u8& = default;
+        constexpr auto operator=(u8&& other) noexcept -> u8& = default;
 
-        inline constexpr u8 lucy_unreliable() noexcept{}
+        constexpr u8 lucy_unreliable() noexcept{}
 
-        inline constexpr u8 lucy_unreliable(const unsigned char value) noexcept : m_value(value){}
+        constexpr u8 lucy_unreliable(const unsigned char value) noexcept : m_value(value){}
 
-        [[nodiscard]] inline constexpr operator bool() const noexcept
+        [[nodiscard]] constexpr operator bool() const noexcept
         {
             return static_cast<bool>(m_value);
         }
 
-        [[nodiscard]] explicit inline constexpr operator s8() const noexcept;
+        [[nodiscard]] explicit constexpr operator s8() const noexcept;
 
-        [[nodiscard]] explicit inline constexpr operator u16() const noexcept;
+        [[nodiscard]] explicit constexpr operator u16() const noexcept;
 
-        [[nodiscard]] explicit inline constexpr operator s16() const noexcept;
+        [[nodiscard]] explicit constexpr operator s16() const noexcept;
 
-        [[nodiscard]] explicit inline constexpr operator f16() const noexcept;
+        [[nodiscard]] explicit constexpr operator f16() const noexcept;
 
-        [[nodiscard]] explicit inline constexpr operator u32() const noexcept;
+        [[nodiscard]] explicit constexpr operator u32() const noexcept;
 
-        [[nodiscard]] explicit inline constexpr operator s32() const noexcept;
+        [[nodiscard]] explicit constexpr operator s32() const noexcept;
 
-        [[nodiscard]] explicit inline constexpr operator f32() const noexcept;
+        [[nodiscard]] explicit constexpr operator f32() const noexcept;
 
-        [[nodiscard]] explicit inline constexpr operator u64() const noexcept;
+        [[nodiscard]] explicit constexpr operator u64() const noexcept;
 
-        [[nodiscard]] explicit inline constexpr operator s64() const noexcept;
+        [[nodiscard]] explicit constexpr operator s64() const noexcept;
 
-        [[nodiscard]] explicit inline constexpr operator f64() const noexcept;
+        [[nodiscard]] explicit constexpr operator f64() const noexcept;
 
-        [[nodiscard]] inline constexpr auto operator+() const noexcept -> u8
+        [[nodiscard]] constexpr auto operator+() const noexcept -> u8
         {
             return u8 lucy_unsafe(m_value);
         }
 
-        inline constexpr auto operator++() noexcept -> u8&
+        constexpr auto operator++() noexcept -> u8&
         {
             ++m_value;
             return *this;
         }
 
-        inline constexpr auto operator++(int) noexcept -> u8
+        constexpr auto operator++(int) noexcept -> u8
         {
             return u8 lucy_unsafe(m_value++);
         }
 
-        inline constexpr auto operator--() noexcept -> u8&
+        constexpr auto operator--() noexcept -> u8&
         {
             --m_value;
             return *this;
         }
 
-        inline constexpr auto operator--(int) noexcept -> u8
+        constexpr auto operator--(int) noexcept -> u8
         {
             return u8 lucy_unsafe(m_value--);
         }
 
-        [[nodiscard]] inline constexpr auto operator==(const u8 other) const noexcept -> bool
+        [[nodiscard]] constexpr auto operator==(const u8 other) const noexcept -> bool
         {
             return m_value == other.m_value;
         }
 
-        [[nodiscard]] inline constexpr auto operator not_eq(const u8 other) const noexcept -> bool
+        [[nodiscard]] constexpr auto operator not_eq(const u8 other) const noexcept -> bool
         {
             return m_value not_eq other.m_value;
         }
 
-        [[nodiscard]] inline constexpr auto operator<(const u8 other) const noexcept -> bool
+        [[nodiscard]] constexpr auto operator<(const u8 other) const noexcept -> bool
         {
             return m_value < other.m_value;
         }
 
-        [[nodiscard]] inline constexpr auto operator>(const u8 other) const noexcept -> bool
+        [[nodiscard]] constexpr auto operator>(const u8 other) const noexcept -> bool
         {
             return m_value > other.m_value;
         }
 
-        [[nodiscard]] inline constexpr auto operator<=(const u8 other) const noexcept -> bool
+        [[nodiscard]] constexpr auto operator<=(const u8 other) const noexcept -> bool
         {
             return m_value <= other.m_value;
         }
 
-        [[nodiscard]] inline constexpr auto operator>=(const u8 other) const noexcept -> bool
+        [[nodiscard]] constexpr auto operator>=(const u8 other) const noexcept -> bool
         {
             return m_value >= other.m_value;
         }
 
-        [[nodiscard]] inline constexpr auto operator<=>(const u8 other) const noexcept -> Comparison
+        [[nodiscard]] constexpr auto operator<=>(const u8 other) const noexcept -> Comparison
         {
             using enum Comparison;
             if(m_value < other.m_value)
@@ -155,122 +155,122 @@ namespace Lucy
             return Equal;
         }
 
-        inline constexpr auto operator and_eq(const u8 other) noexcept -> u8&
+        constexpr auto operator and_eq(const u8 other) noexcept -> u8&
         {
             m_value and_eq other.m_value;
             return *this;
         }
 
-        inline constexpr auto operator or_eq(const u8 other) noexcept -> u8&
+        constexpr auto operator or_eq(const u8 other) noexcept -> u8&
         {
             m_value or_eq other.m_value;
             return *this;
         }
 
-        inline constexpr auto operator xor_eq(const u8 other) noexcept -> u8&
+        constexpr auto operator xor_eq(const u8 other) noexcept -> u8&
         {
             m_value xor_eq other.m_value;
             return *this;
         }
 
-        inline constexpr auto operator<<=(const u8 shifts) noexcept -> u8&
+        constexpr auto operator<<=(const u8 shifts) noexcept -> u8&
         {
             m_value <<= shifts.m_value;
             return *this;
         }
 
-        inline constexpr auto operator>>=(const u8 shifts) noexcept -> u8&
+        constexpr auto operator>>=(const u8 shifts) noexcept -> u8&
         {
             m_value >>= shifts.m_value;
             return *this;
         }
 
-        inline constexpr auto operator+=(const u8 other) noexcept -> u8&
+        constexpr auto operator+=(const u8 other) noexcept -> u8&
         {
             m_value += other.m_value;
             return *this;
         }
 
-        inline constexpr auto operator-=(const u8 other) noexcept -> u8&
+        constexpr auto operator-=(const u8 other) noexcept -> u8&
         {
             m_value -= other.m_value;
             return *this;
         }
 
-        inline constexpr auto operator*=(const u8 other) noexcept -> u8&
+        constexpr auto operator*=(const u8 other) noexcept -> u8&
         {
             m_value *= other.m_value;
             return *this;
         }
 
-        inline constexpr auto operator/=(const u8 other) noexcept -> u8&
+        constexpr auto operator/=(const u8 other) noexcept -> u8&
         {
             m_value /= other.m_value;
             return *this;
         }
 
-        inline constexpr auto operator%=(const u8 other) noexcept -> u8&
+        constexpr auto operator%=(const u8 other) noexcept -> u8&
         {
             m_value %= other.m_value;
             return *this;
         }
 
-        [[nodiscard]] inline constexpr auto operator compl() const noexcept -> u8
+        [[nodiscard]] constexpr auto operator compl() const noexcept -> u8
         {
             return u8 lucy_unsafe(compl m_value);
         }
 
-        [[nodiscard]] inline constexpr auto operator bitand(const u8 other) const noexcept -> u8
+        [[nodiscard]] constexpr auto operator bitand(const u8 other) const noexcept -> u8
         {
             return u8 lucy_unsafe(m_value bitand other.m_value);
         }
 
-        [[nodiscard]] inline constexpr auto operator bitor(const u8 other) const noexcept -> u8
+        [[nodiscard]] constexpr auto operator bitor(const u8 other) const noexcept -> u8
         {
             return u8 lucy_unsafe(m_value bitor other.m_value);
         }
 
-        [[nodiscard]] inline constexpr auto operator xor(const u8 other) const noexcept -> u8
+        [[nodiscard]] constexpr auto operator xor(const u8 other) const noexcept -> u8
         {
             return u8 lucy_unsafe(m_value xor other.m_value);
         }
 
-        [[nodiscard]] inline constexpr auto operator<<(const u8 other) const noexcept -> u8
+        [[nodiscard]] constexpr auto operator<<(const u8 other) const noexcept -> u8
         {
             return u8 lucy_unsafe(m_value << other.m_value);
         }
 
-        [[nodiscard]] inline constexpr auto operator>>(const u8 other) const noexcept -> u8
+        [[nodiscard]] constexpr auto operator>>(const u8 other) const noexcept -> u8
         {
             return u8 lucy_unsafe(m_value >> other.m_value);
         }
 
-        [[nodiscard]] inline constexpr auto operator+(const u8 other) const noexcept -> u8
+        [[nodiscard]] constexpr auto operator+(const u8 other) const noexcept -> u8
         {
             return u8 lucy_unsafe(m_value + other.m_value);
         }
 
-        [[nodiscard]] inline constexpr auto operator-(const u8 other) const noexcept -> u8
+        [[nodiscard]] constexpr auto operator-(const u8 other) const noexcept -> u8
         {
             return u8 lucy_unsafe(m_value - other.m_value);
         }
 
-        [[nodiscard]] inline constexpr auto operator*(const u8 other) const noexcept -> u8
+        [[nodiscard]] constexpr auto operator*(const u8 other) const noexcept -> u8
         {
             return u8 lucy_unsafe(m_value * other.m_value);
         }
 
-        [[nodiscard]] inline constexpr auto operator/(const u8 other) const noexcept -> u8
+        [[nodiscard]] constexpr auto operator/(const u8 other) const noexcept -> u8
         {
             return u8 lucy_unsafe(m_value / other.m_value);
         }
 
-        [[nodiscard]] inline constexpr auto operator%(const u8 other) const noexcept -> u8
+        [[nodiscard]] constexpr auto operator%(const u8 other) const noexcept -> u8
         {
             return u8 lucy_unsafe(m_value % other.m_value);
         }
 
-        [[nodiscard]] inline constexpr auto Value() const noexcept -> unsigned char
+        [[nodiscard]] constexpr auto Value() const noexcept -> unsigned char
         {
             return m_value;
         }
@@ -280,7 +280,7 @@ namespace Lucy
     {
         void LiteralIsBiggerThan255() noexcept;
 
-        [[nodiscard]] inline consteval auto operator ""_u8(const unsigned long long int value) noexcept -> u8
+        [[nodiscard]] consteval auto operator ""_u8(const unsigned long long int value) noexcept -> u8
         {
             if(value > 255ull)
                 LiteralIsBiggerThan255();
