3e83a1b18e432eecbde2c252483fca92a0ab2290
raygardnercpp <samuelalonso26@icloud.com>
2024-08-22 15:51:53 +0100

    Added Meta Same

---
+++ Source/Meta/Same.hpp
@@ -0,0 +1,11 @@
+#if lucy_major >= 0 and lucy_middle >= 0 and lucy_minor >= 1
+#ifndef lucy_meta_same
+#define lucy_meta_same
+
+namespace Lucy::Meta
+{
+    template<class T, class U> concept Same = __is_same(T, U);
+}
+
+#endif
+#endif
