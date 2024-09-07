8ef1d71b0e34166a76b7b3b37ec095efb3bd4a8a
raygardnercpp <samuelalonso26@icloud.com>
2024-09-07 22:53:41 +0100

    Refactored Result

--- Source/Core/Result.hpp
+++ Source/Core/Result.hpp
@@ -13,7 +13,7 @@ namespace Lucy
     {
         template<class...> class [[nodiscard]] ResultH{};
 
-        template<Size> class [[nodiscard]] ResultCopy final{};
+        template<bool> class [[nodiscard]] ResultCopy final{};
 
         template<class T, class... U>  class [[nodiscard]] ResultH<T, U...>
         {
@@ -25,16 +25,16 @@ namespace Lucy
                 ResultH<U...> m_tail;
             };
 
-            constexpr ~ResultH() noexcept{}
-
-            constexpr ~ResultH() noexcept requires(Meta::TriviallyCopyable<T>)
+            constexpr ~ResultH() noexcept
             {
                 m_actual.~T();
             }
 
-            template<Size index, class... Args> constexpr ResultH(ResultCopy<index>, Args&&... args) noexcept : m_tail(ResultH<U...>(ResultCopy<index - 1uz>{}, lucy_forward(args)...)){}
+            constexpr ~ResultH() noexcept requires(Meta::TriviallyCopyable<T>){}
 
-            template<class... Args> constexpr ResultH(ResultCopy<0uz>, Args&&... args) noexcept : m_actual(lucy_forward(args)...){}
+            template<class... Args> constexpr ResultH(ResultCopy<true>, Args&&... args) noexcept : m_tail(ResultH<U...>(ResultCopy<false>{}, lucy_forward(args)...)){}
+
+            template<class... Args> constexpr ResultH(ResultCopy<false>, Args&&... args) noexcept : m_actual(lucy_forward(args)...){}
         };
 
         template<bool, class, class T> class ResultConditional
@@ -49,9 +49,9 @@ namespace Lucy
             using Type = T;
         };
 
-        template<Size index, class T, class U> constexpr auto Value(ResultH<T, U>& result) noexcept -> ResultConditional<index == 0uz, T, U>::Type&
+        template<bool index, class T, class U> constexpr auto Value(ResultH<T, U>& result) noexcept -> ResultConditional<not index, T, U>::Type&
         {
-            if constexpr(index == 0u)
+            if constexpr(not index)
                 return result.m_actual;
             else
                 return result.m_tail.m_actual;
@@ -70,15 +70,15 @@ namespace Lucy
     public:
         constexpr ~Result() noexcept = default;
 
-        constexpr Result() noexcept : m_error(false), m_variant(Detail::ResultCopy<0uz>{}){}
+        constexpr Result() noexcept : m_error(false), m_variant(Detail::ResultCopy<false>{}){}
 
-        template<class... Arguments> requires(Meta::Constructible<T, Arguments...>) constexpr Result(Arguments&&... arguments) noexcept : m_error(false), m_variant(Detail::ResultCopy<0uz>{}, lucy_forward(arguments)...){}
+        template<class... Arguments> requires(Meta::Constructible<T, Arguments...>) constexpr Result(Arguments&&... arguments) noexcept : m_error(false), m_variant(Detail::ResultCopy<false>{}, lucy_forward(arguments)...){}
 
-        template<class... Arguments> requires(Meta::Constructible<ErrorT, Arguments...>) constexpr Result(Arguments&&... arguments) noexcept : m_error(true), m_variant(Detail::ResultCopy<1uz>{}, lucy_forward(arguments)...){}
+        template<class... Arguments> requires(Meta::Constructible<ErrorT, Arguments...>) constexpr Result(Arguments&&... arguments) noexcept : m_error(true), m_variant(Detail::ResultCopy<true>{}, lucy_forward(arguments)...){}
 
-        template<class... Arguments> requires(Meta::Constructible<T, Arguments...>) constexpr Result(ConstructT, Arguments&&... arguments) noexcept : m_error(false), m_variant(Detail::ResultCopy<0uz>{}, lucy_forward(arguments)...){}
+        template<class... Arguments> requires(Meta::Constructible<T, Arguments...>) constexpr Result(ConstructT, Arguments&&... arguments) noexcept : m_error(false), m_variant(Detail::ResultCopy<false>{}, lucy_forward(arguments)...){}
 
-        template<class... Arguments> requires(Meta::Constructible<ErrorT, Arguments...>) constexpr Result(ConstructError, Arguments&&... arguments) noexcept : m_error(true), m_variant(Detail::ResultCopy<1uz>{}, lucy_forward(arguments)...){}
+        template<class... Arguments> requires(Meta::Constructible<ErrorT, Arguments...>) constexpr Result(ConstructError, Arguments&&... arguments) noexcept : m_error(true), m_variant(Detail::ResultCopy<true>{}, lucy_forward(arguments)...){}
 
         template<class Argument> requires(Meta::Constructible<T, Argument>) constexpr auto operator=(Argument&& argument) noexcept -> Result<T, ErrorT>&
         {
